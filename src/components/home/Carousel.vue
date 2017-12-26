3143038dd0e8710f4ce3e344f16c7358bf812b66
NKabiru <njengakabiru@gmail.com>
2017-12-26 12:35:08 +0300

    Fix carousel image height

--- src/components/home/Carousel.vue
+++ src/components/home/Carousel.vue
@@ -7,33 +7,33 @@
             </div>
             <ul class="orbit-container">
                 <li class="orbit-slide">
-                    <figure class="orbit-figure"><img src="https://placehold.it/1200x600/999?text=Slide-1" alt="" class="orbit-image">
+                    <figure class="orbit-figure"><img :src="gameImageUrl[0]" alt="" class="orbit-image">
                         <figcaption class="orbit-caption">Sample caption</figcaption>
                     </figure>
                 </li>
                 <li class="orbit-slide">
-                    <figure class="orbit-figure"><img src="https://placehold.it/1200x600/999?text=Slide-4" alt="" class="orbit-image">
+                    <figure class="orbit-figure"><img :src="gameImageUrl[1]" alt="" class="orbit-image">
                         <figcaption class="orbit-caption">Sample caption</figcaption>
                     </figure>
                 </li>
                 <li class="orbit-slide">
-                    <figure class="orbit-figure"><img src="https://placehold.it/1200x600/999?text=Slide-2" alt="" class="orbit-image">
+                    <figure class="orbit-figure"><img :src="gameImageUrl[2]" alt="" class="orbit-image">
                         <figcaption class="orbit-caption">Sample caption</figcaption>
                     </figure>
                 </li>
                 <li class="orbit-slide">
-                    <figure class="orbit-figure"><img src="https://placehold.it/1200x600/999?text=Slide-3" alt="" class="orbit-image">
+                    <figure class="orbit-figure"><img :src="gameImageUrl[3]" alt="" class="orbit-image">
                         <figcaption class="orbit-caption">Sample caption</figcaption>
                     </figure>
                 </li>
                 <li class="orbit-slide">
-                    <figure class="orbit-figure"><img src="https://placehold.it/1200x600/999?text=Slide-2" alt="" class="orbit-image">
+                    <figure class="orbit-figure"><img :src="gameImageUrl[4]" alt="" class="orbit-image">
                         <figcaption class="orbit-caption">Sample caption</figcaption>
                     </figure>
                 </li>
             </ul>
         </div>
-        <button class="button" type="button" @click="getGame(carouselGameIDs[0])">View API</button>
+        <!--<button class="button" type="button" @click="getGame(carouselGameIDs[0])">View API</button>-->
     </div>
 </template>
 
@@ -44,23 +44,29 @@
 
     export default {
         name: "carousel",
-        data(){
-            return{
-                carouselGameIDs: ['3030-54134','3030-59203','3030-52647','3030-58573','3030-54144']
+        data() {
+            return {
+                carouselGameIDs: ['3030-54134', '3030-59203', '3030-52647', '3030-58573', '3030-54144'],
+                gameImageUrl: []
             }
         },
-        methods: {
-            getGame(gameID){
+        mounted(){
+            for(let i = 0; i < this.carouselGameIDs.length; i++){
                 m.jsonp({
-                    url: `${ baseUrl }/game/${ gameID }/`,
+                    url: `${ baseUrl }/game/${ this.carouselGameIDs[i] }/`,
                     data: {
                         api_key: apiKey,
                         format: 'jsonp',
                         field_list: 'name,deck,image'
                     },
                     callbackKey: 'json_callback'
-                }).then(game => console.log(game))
+                }).then(game => {
+                    this.gameImageUrl.push(game.results.image.screen_large_url)
+                })
             }
+        },
+        methods: {
+
         }
     }
 </script>
