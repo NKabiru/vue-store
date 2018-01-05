3a103a7dd0c00d15002a49add49109089fe15bae
NKabiru <njengakabiru@gmail.com>
2018-01-05 21:03:45 +0300

    Add API search request to retrieve games from API

--- src/components/navbar/AddGameModal.vue
+++ src/components/navbar/AddGameModal.vue
@@ -2,27 +2,66 @@
     <div class="reveal" id="addGameModal">
         <h4>Add New Game</h4>
         <div class="grid-x">
-            <div class="small-12">
-                <label>
-                    Search
-                    <input type="text">
-                </label>
+            <div class="small-12 cell">
+                <div class="input-group">
+                    <input class="input-group-field" type="text" v-model="searchGame" placeholder="Search Game...">
+                    <div class="input-group-button">
+                        <button class="small button" @click="searchGameFromApi"><icon name="search"></icon></button>
+                    </div>
+                </div>
             </div>
-            <div class="small-12"></div>
-        </div>
-        <div class="grid-x">
-            <button class="small button">Add</button>
+            <div class="small-12 cell">
+                <template v-if="searchResults.length >= 1">
+                    <p>Search Results:</p>
+                    <table>
+                        <tbody>
+                            <tr v-for="game in searchResults">
+                                <td><img class="search-result-icon" :src="game.image.icon_url" alt=""></td>
+                                <td>{{ game.name }}</td>
+                                <td><button class="tiny hollow button">Add To Catalog</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </template>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import {apiKey} from "../../api/config";
+    import {baseUrl} from "../../api";
+    import Icon from "vue-awesome/components/Icon";
+    import 'vue-awesome/icons/plus-circle'
+    import 'vue-awesome/icons/search';
+    let m = require('mithril');
+
     export default {
         name: "add-game-modal",
 
         data(){
             return{
+                searchGame: '',
+                searchResults: []
+            }
+        },
 
+        methods:{
+            searchGameFromApi(){
+                m.jsonp({
+                    url: `${ baseUrl }/search/`,
+                    data: {
+                        api_key: apiKey,
+                        format: 'jsonp',
+                        limit: 5,
+                        field_list: 'name,deck,image,guid',
+                        resources:'game',
+                        query: this.searchGame
+                    },
+                    callbackKey: 'json_callback'
+                }).then(game => {
+                    this.searchResults = game.results
+                });
             }
         },
 
@@ -32,7 +71,11 @@
 
         destroyed(){
             this.reveal.destroy();
-        }
+        },
+
+        components: {
+            Icon
+        },
     }
 </script>
 
