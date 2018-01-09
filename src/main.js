7a8852f35d15156cdc5da67c9eb76cf4d34948d2
NKabiru <njengakabiru@gmail.com>
2018-01-09 12:40:46 +0300

    Add loading GIF for lazy-loading images

--- src/main.js
+++ src/main.js
@@ -9,10 +9,13 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import locale from 'element-ui/lib/locale/lang/en'
 import VueLazyLoad from 'vue-lazyload'
+import loadingGif from './assets/loading.gif'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 
-Vue.use(VueLazyLoad);
+Vue.use(VueLazyLoad, {
+  loading: loadingGif
+});
 Vue.use(ElementUI, { locale });
 
 Vue.config.productionTip = false;
