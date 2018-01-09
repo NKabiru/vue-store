92bca8e1479454cab63c61a97825abcb2f0c68e1
NKabiru <njengakabiru@gmail.com>
2018-01-09 11:27:23 +0300

    Remove api call from api/index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,26 +1,6 @@
 import m from 'mithril';
 import { apiKey } from "./config";
 
-const baseUrl = 'https://www.giantbomb.com/api'
+const baseUrl = 'https://www.giantbomb.com/api';
 
-var games = [];
-
-function getGames() {
-    m.jsonp({
-        url: `${ baseUrl }/games/`,
-        data: {
-            api_key: apiKey,
-            format: 'jsonp',
-            field_list: 'name,deck,image,id',
-            filter:'id:59203|52647|58573'
-        },
-        callbackKey: 'json_callback'
-    }).then(game => {
-        games = game.results
-        return games;
-    })
-}
-
-
-
-export { baseUrl, getGames }
+export { baseUrl }
