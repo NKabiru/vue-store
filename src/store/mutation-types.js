d3d0ae9c4366925b8c0629d8e0e34f037181a141
NKabiru <njengakabiru@gmail.com>
2018-01-11 16:24:31 +0300

    Add more mutation types

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -2,4 +2,7 @@ export const ADD_TO_CART = 'ADD_TO_CART';
 export const ADD_TO_CATALOG = 'ADD_TO_CATALOG';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
 export const DROP_ALL_CART_ITEMS = 'DROP_ALL_CART_ITEMS';
+export const LOGIN = 'LOGIN';
+export const LOGOUT = 'LOGOUT';
+export const LOGIN_SUCCESS = 'LOGOUT_SUCCESS';
 
