5277324b5e8af9073355a9c23afea6e872351d00
NKabiru <njengakabiru@gmail.com>
2018-01-31 10:20:01 +0300

    Add carousel images

--- src/components/home/Carousel.vue
+++ src/components/home/Carousel.vue
@@ -1,20 +1,20 @@
 <template>
-
+    <v-carousel hide-delimiters>
+        <v-carousel-item v-for="(game,i) in carouselGames" v-bind:src="game.src" :key="i"></v-carousel-item>
+    </v-carousel>
 </template>
 
 <script>
-    import ElCarousel from "element-ui/packages/carousel/src/main"
-    import ElCarouselItem from "element-ui/packages/carousel/src/item"
-    import { apiKey } from "../../api/config";
-    import { baseUrl } from "../../api";
-    let m = require('mithril');
-
     export default {
         name: "carousel-component",
 
         data() {
             return {
-
+                carouselGames: [
+                    {src: '/static/img/carousel-destiny2.jpg'},
+                    {src: '/static/img/carousel-callofdutyww2.jpg'},
+                    {src: '/static/img/carousel-shadowofwar.jpg'}
+                ]
             }
         },
 
@@ -24,11 +24,6 @@
 
         methods: {
 
-        },
-
-        components: {
-            ElCarousel,
-            ElCarouselItem
         }
     }
 </script>
